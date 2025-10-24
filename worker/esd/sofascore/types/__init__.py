f91ed3957c812302a8897c3a004b42767668c066
atho-gitrepo <aungthuheinoo1992@gmail.com>
2025-10-25 01:30:48 +0630

    Update __init__.py

--- worker/esd/sofascore/types/__init__.py
+++ worker/esd/sofascore/types/__init__.py
@@ -5,20 +5,53 @@ Contains the types for the Sofascore service.
 """
 
 from .event import Event, parse_events, parse_event
-# ... (all other imports) ...
+from .team import Team, parse_team, TeamTournamentStats, parse_team_tournament_stats
+from .player import Player, parse_player
+from .country import Country, parse_country
+from .color import Color, parse_color
+from .manager import Manager, parse_manager
+from .transfer_history import TransferHistory, parse_transfer_history
+from .player_attributes import PlayerAttributes, parse_player_attributes
+from .match_stats import MatchStats, parse_match_stats
+from .lineups import Lineups, parse_lineups
+from .incident import Incident, parse_incidents
+from .top_players_match import TopPlayersMatch, parse_top_players_match
+from .comment import Comment, parse_comments
+from .shot import Shot, parse_shots
+from .tournament import Tournament, parse_tournament, parse_tournaments
+from .season import Season, parse_seasons
+from .bracket import Bracket, parse_brackets
+from .standing import Standing, parse_standings
+from .top_teams import TopTournamentTeams, parse_top_tournament_teams
+from .top_players import TopTournamentPlayers, parse_top_tournament_players
 from .entity import EntityType
 from .categories import Category
-# 🟢 INTEGRATION: Import the team tournament stats data structure
-from .team_stats import TeamTournamentStats, parse_team_tournament_stats 
+from .status import StatusType, Status
 
 
 __all__ = [
-    # ... (all other symbols) ...
+    "Event", "parse_events", "parse_event", 
+    "Team", "parse_team", 
+    "TeamTournamentStats", "parse_team_tournament_stats",
+    "Player", "parse_player",
+    "Country", "parse_country",
+    "Color", "parse_color",
+    "Manager", "parse_manager",
+    "TransferHistory", "parse_transfer_history",
+    "PlayerAttributes", "parse_player_attributes",
+    "MatchStats", "parse_match_stats",
+    "Lineups", "parse_lineups",
+    "Incident", "parse_incidents",
+    "TopPlayersMatch", "parse_top_players_match",
+    "Comment", "parse_comments",
+    "Shot", "parse_shots",
+    "Tournament", "parse_tournament", "parse_tournaments",
+    "Season", "parse_seasons",
+    "Bracket", "parse_brackets",
+    "Standing", "parse_standings",
+    "TopTournamentTeams", "parse_top_tournament_teams",
+    "TopTournamentPlayers", "parse_top_tournament_players",
     "EntityType",
     "Category",
-    "StatusType",
-    "Status",
-    # 🟢 INTEGRATION: Add the team tournament stats to the exported symbols
-    "TeamTournamentStats",
-    "parse_team_tournament_stats",
+    "StatusType", "Status"
 ]
